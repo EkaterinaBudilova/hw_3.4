82da83c60d9ca3afd10015e5cf3be322defa93c0
EkaterinaBudilova <ekaterinazh1@mail.ru>
2017-12-17 19:33:26 +1200

    Update

--- vkapi.py
+++ vkapi.py
@@ -1,27 +1,38 @@
 from urllib.parse import urlencode
+import requests
 
-AUTH_URL = 'http://oauth.vk.com/authorize'
-APP_ID = 6287762
+def get_token():
+    AUTH_URL = 'http://oauth.vk.com/authorize'
+    APP_ID = 6287762
 
-url_data = {
-    'client_id': APP_ID,
-    'redirect_uri': 'https://oauth.vk.com/blank.html',
-    'display': 'page',
-    'scope': 'friends',
-    'response_type': 'token',
-    'v': '5.69'
-}
+    url_data = {
+        'client_id': APP_ID,
+        'redirect_uri': 'https://oauth.vk.com/blank.html',
+        'display': 'page',
+        'scope': 'friends',
+        'response_type': 'token',
+        'v': '5.69'
+    }
 
-token = '?'.join(
-    (AUTH_URL, urlencode(url_data)
-))
-print(token)
+    token = '?'.join(
+        (AUTH_URL, urlencode(url_data)
+         ))
+    return token
 
-SOURCE_UID = input('Введите ID пользователя: ')
-TARGET_UID = input('Введите ID пользователя: ')
-ACCESS_TOKEN = input('Введите ACCESS_TOKEN пользователя: ')
-FRIENDS_LIST = 'https://api.vk.com/method/friends.get?user_id=307676802&v=5.69'
-print(FRIENDS_LIST)
-NEW_URL = 'https://api.vk.com/method/friends.getMutual?source_uid={}&target_uid={}&access_token={}&v=5.69'.format(
-    SOURCE_UID, TARGET_UID, ACCESS_TOKEN)
-print(NEW_URL)
+def mutual_friends():
+    source_uid = input('Введите ID пользователя: ')
+    target_uid = input('Введите ID пользователя: ')
+    access_token = input('Введите ACCESS_TOKEN пользователя: ')
+    friends_list = 'https://api.vk.com/method/friends.get?user_id=307676802&v=5.69'
+    print(friends_list)
+    new_url = 'https://api.vk.com/method/friends.getMutual?source_uid={}&target_uid={}&access_token={}&v=5.69'.format(
+        source_uid, target_uid, access_token)
+    text = requests.get(url = new_url).json()
+    return text['response']
+
+if __name__ == '__main__':
+    token_url = get_token()
+    print(token_url)
+    friends_list = mutual_friends()
+    for frand_id in friends_list:
+        print(frand_id)
